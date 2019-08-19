ab276eec33d4c29863c39ba358491f44a81c6f57
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-08-19 11:49:32 -0400

    fits2jpeg: initial bits

--- fitsbits/fits2jpeg.py
+++ fitsbits/fits2jpeg.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''This is a script to convert a FITS file to a JPEG from the command-line.
+'''This is a script to export a FITS image to a full JPEG, stamp PNG, or JPEGs
+of specified regions.
+
 '''
 
 #############
@@ -34,3 +36,45 @@ LOGEXCEPTION = LOGGER.exception
 #############
 ## IMPORTS ##
 #############
+
+import os.path
+import sys
+from argparse import ArgumentParser
+
+from .export import (
+    fits_to_full_jpeg,
+    fits_radecbox_to_jpeg,
+    fits_xybox_to_jpeg,
+    fits_to_stamps
+)
+
+
+##########
+## MAIN ##
+##########
+
+def main():
+    '''
+    This is the main function.
+
+    '''
+
+    aparser = ArgumentParser(
+        description=('Convert a FITS image into a JPEG, make stamp PNGs, '
+                     'or generate JPEGs of specified regions in a FITS image.')
+    )
+
+    aparser.add_argument(
+        'fitsfile',
+        action='store',
+        type=str,
+        help=("Path to the FITS file to operate on.")
+    )
+
+    aparser.add_argument(
+        'operation',
+        action='store',
+        choices=['jpeg','stamp','radecbox','xybox'],
+        type=str,
+        help=("Operation to perform.")
+    )
